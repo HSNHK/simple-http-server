0bf173ade8a61f0f01761b2837930d4ac9136958
HSNHK <hsnmm1379@gmail.com>
2021-01-31 17:04:24 +0330

    create server

--- src/server.py
+++ src/server.py
@@ -1,112 +1,84 @@
+from .header import Generate,Assembler
+from datetime import datetime
+from logging import DEBUG
+from .log import Log
+import threading
 import socket
 import sys
-from datetime import datetime
-import threading
-import signal
-import json
-
 
 class web_server:
-    def __init__(self,port=8080,route={}):
-        self.HOST="127.0.0.1"
+    def __init__(self,host="127.0.0.1",port=8080,route={},DEBUG=False):
+        self.HOST=host
         self.PORT=port
         self.route_table=route
-    def start(self):
+        if DEBUG:
+            self.Log=Log(Name="Server logging",level=DEBUG)
+        else:
+            self.Log=Log("Server logging")
+        print("* Welcome to the web server\n" +
+              "* debug mode : ",DEBUG)
+        
+    def run(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             self.socket.bind((self.HOST,self.PORT))
-            print("Start Webserver with :%s:%s "%(self.HOST,self.PORT))
+            self.Log.info(message="Start Webserver with :%s:%s "%(self.HOST,self.PORT))
         except:
-            print("Difficult to run\nThis port is probably reserved")
+            self.Log.critical("Difficult to run. This port is probably reserved")
             self.shutdown()
 
-        self.listen()
+        self.__listen()
 
     def shutdown(self):
         self.socket.shutdown(socket.SHUT_RDWR)
         sys.exit(1)
 
-    def generate_headers(self,status_code,contentType):
-        header=""
-        if status_code==200:
-            header+="HTTP/1.1 200 OK\n"
-        elif status_code==404:
-            header+="HTTP/1.1 404 Not Found\n"
-            
-        header+="Access-Control-Allow-Origin: *\n"
-        header+="Access-Control-Allow-Headers: Content-Type, Set-Cookie, *\n"
-        header+="Set-Cookie: session=344772952; Domain=0.0.0.0; expires=Fri, 19-Jan-2018 20:04:36 PST; Path=/\n"
-        
-        header+="Date :%s\n"%datetime.now()
-        header+="Server :First webserver\n"
-
-        if contentType=="json":
-            header+="Content-type :application/json\n"
-        elif contentType=="img":
-            header += "Content-type :image/png\n"
-        else:
-            header += "Content-type :text/plain\n"
-
-        header+="Connection :close \n\n"
-
-        return header
-    
-    def listen(self):
+    def __listen(self):
         self.socket.listen(5)
         try:
             while True:
                 (client,address)=self.socket.accept()
                 client.settimeout(60)
-                print("connect new client %s",address)
-                threading.Thread(target=self.handling,args=(client,address)).start()
+                self.Log.info(message="connect new client Host : %s Port : %d"%(address[0],address[1]))
+                threading.Thread(target=self.__handling,args=(client,address)).start()
         except Exception as ex:
-            print(ex)
+            self.Log.error("server listen error : %s"%ex)
 
-    def handling(self,client,addres):
+    def __send(self,request,header:str,data:str):
+        response=header.encode()
+        response+=data
+        request.send(response)
+        request.close()
+
+    def __handling(self,client,addres):
         PACKET_SIZE=1024
         while True:
-            data = client.recv(PACKET_SIZE).decode()
+            try:
+                data = client.recv(PACKET_SIZE).decode()
 
-            if not data:
+                if not data:
+                    break
+                request_data=Assembler(data)
+
+                self.Log.info("The request: |%s| |%s| |%s| |%s|"%("{}:{}".format(addres[0],addres[1]),
+                                request_data.get_method,
+                                request_data.get_parameter,
+                                str(datetime.now())))
+
+                if request_data.get_parameter in self.route_table:
+                    function=self.route_table[request_data.get_parameter](request_data)
+                    DATA=function["data"]
+                    HEADER=Generate(status_code=200,content_type=function["type"])
+                    self.__send(client,HEADER.generate_header(),DATA)
+                    break
+                else:
+                    HEADER=Generate(status_code=404)
+                    self.__send(client,HEADER.generate_header(),"<br>Not Found 404</br>".encode())
+                    break
+            except Exception as ex:
+                self.Log.error("Internal Server Error : "+ex)
+                HEADER=Generate(status_code=500)
+                self.__send(client, HEADER.generate_header(),
+                            "<h1>Internal Server Error 500</h1>".encode())
                 break
-
-            request_data=data.split(' ')
-            print("request method : %s"%request_data[0])
-            if request_data[1] in self.route_table:
-                func=self.route_table[request_data[1]]()
-                response_data=func["data"]
-                response=self.generate_headers(200,func["type"]).encode()
-                response+=response_data
-                client.send(response)
-                client.close()
-                break
-            else:
-                response_data="not found"
-                response=self.generate_headers(404,"").encode()
-                response+=json.dumps(response_data).encode()
-                client.send(response)
-                client.close()
-                break
-
-def shutdown(sing,unused):
-    server.shutdown()
-    sys.exit(1)
-
-def index()->dict:
-    return {"data":b"index ...","type":""}
-
-def programer()->dict:
-    return {"data":b"hello im hasan hk","type":""}
-
-def fav()->dict:
-    file=open("922844.png","rb")
-    return {"data":file.read(),"type":"img"}
-
-signal.signal(signal.SIGINT, shutdown)
-
-urlpatterns={"/":index,
-    "/programer":programer,
-    "/favicon.ico":fav}
-server=web_server(port=8081,route=urlpatterns)
-server.start()
 
