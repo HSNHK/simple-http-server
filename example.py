2589106634ef0e0eb21e838ad9e3d5ced783d43b
HSNHK <hsnmm1379@gmail.com>
2021-03-03 23:03:55 +0330

    Specify the protocol in routing and no need to convert data

--- example.py
+++ example.py
@@ -6,26 +6,26 @@ def shutdown(sing,unused):
     server.shutdown()
     sys.exit(1)
 
-def index(request:Assembler)->dict:
+def index(request:Assembler,client)->dict:
     if request.get_method=="GET":
-        return {"data":b"welcome to index page","type":"plain"}
+        return {"data":"welcome to index page","type":"plain"}
     else:
-        return {"data": b"not protocol supported", "type": "plain"}
+        return {"data":"not protocol supported", "type": "plain"}
 
-def programer(request:Assembler)->dict:
-    return {"data":b"""<center>hello im hasan hk<br>
+def programer(request:Assembler,client)->dict:
+    return {"data":"""<center>hello im hasan hk<br>
             You can follow me through my GitHub account<br>
             <img src=\"https://github.com/HSNHK.png?size=100\"><br>
             <a href=\"https://github.com/HSNHK\">GitHub Page</a>""", "type": "html"}
 
-def fav(request: Assembler) -> dict:
+def fav(request: Assembler,client) -> dict:
     file=open("fav.png","rb")
     return {"data":file.read(),"type":"img"}
 
 signal.signal(signal.SIGINT, shutdown)
 
 urlpatterns={"/":index,
-    "/programer":programer,
+    "/programer":[programer,"POST"],
     "/favicon.ico":fav}
 
 server=web_server(port=80,route=urlpatterns)
--- src/server.py
+++ src/server.py
@@ -15,14 +15,14 @@ class web_server:
             self.Log=Log(Name="Server logging",level=DEBUG)
         else:
             self.Log=Log("Server logging")
-        print("* Welcome to the web server\n" +
+        print("* Welcome to the web server\n" \
               "* debug mode : ",DEBUG)
         
     def run(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             self.socket.bind((self.HOST,self.PORT))
-            self.Log.info(message="Start Webserver with :%s:%s "%(self.HOST,self.PORT))
+            self.Log.info("*Start Webserver with :%s:%s "%(self.HOST,self.PORT))
         except:
             self.Log.critical("Difficult to run. This port is probably reserved")
             self.shutdown()
@@ -39,7 +39,7 @@ class web_server:
             while True:
                 (client,address)=self.socket.accept()
                 client.settimeout(60)
-                self.Log.info(message="connect new client Host : %s Port : %d"%(address[0],address[1]))
+                self.Log.info("connect new client Host : %s Port : %d"%(address[0],address[1]))
                 threading.Thread(target=self.__handling,args=(client,address)).start()
         except Exception as ex:
             self.Log.error("server listen error : %s"%ex)
@@ -66,19 +66,41 @@ class web_server:
                                 str(datetime.now())))
 
                 if request_data.get_parameter in self.route_table:
-                    function=self.route_table[request_data.get_parameter](request_data)
+                    if type(self.route_table[request_data.get_parameter]) is list:
+                        Method=self.route_table[request_data.get_parameter][1]
+                        if request_data.get_method.casefold()!=Method.casefold():
+                             self.protocol_not_supported(client)
+                             break
+                        function=self.route_table[request_data.get_parameter][0](request_data,client)
+                    else:
+                        function=self.route_table[request_data.get_parameter](request_data,client)
+
                     DATA=function["data"]
+                    if type(DATA)!=bytes:
+                        DATA=str(DATA).encode()
+
                     HEADER=Generate(status_code=200,content_type=function["type"])
                     self.__send(client,HEADER.generate_header(),DATA)
                     break
                 else:
-                    HEADER=Generate(status_code=404)
-                    self.__send(client,HEADER.generate_header(),"<br>Not Found 404</br>".encode())
+                    self.not_found(client)
                     break
             except Exception as ex:
-                self.Log.error("Internal Server Error : "+ex)
-                HEADER=Generate(status_code=500)
-                self.__send(client, HEADER.generate_header(),
-                            "<h1>Internal Server Error 500</h1>".encode())
+                self.Log.error("Internal Server Error : ",ex)
+                self.internal_error(client)
                 break
+    
+    def not_found(self,client):
+        HEADER = Generate(status_code=404, content_type="html")
+        self.__send(client, HEADER.generate_header(),
+                    "<br>Not Found 404</br>".encode())
 
+    def internal_error(self,client):
+        HEADER = Generate(status_code=500, content_type="html")
+        self.__send(client, HEADER.generate_header(),
+                    "<h1>Internal Server Error 500</h1>".encode())
+
+    def protocol_not_supported(self,client):
+        HEADER = Generate(status_code=404, content_type="html")
+        self.__send(client, HEADER.generate_header(),
+                    "<br>Protocol Not Supported</br>".encode())
